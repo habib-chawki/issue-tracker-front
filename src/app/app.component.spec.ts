faf98b6db82c3ebe7ae4f70e8aabf244816764c4
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-07 13:01:15 +0100

    Test issues component rendering

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { IssuesComponent } from './components/issues/issues.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        IssuesComponent
       ],
     }).compileComponents();
   });
@@ -31,5 +33,11 @@ describe('AppComponent', () => {
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('App is running');
+  });
+
+  it('should render issues component', () =>{
+    const fixture = TestBed.createComponent(AppComponent);
+    const nativeElement = fixture.nativeElement;
+    expect(nativeElement.querySelector('app-issues')).not.toBeNull();
   });
 });
