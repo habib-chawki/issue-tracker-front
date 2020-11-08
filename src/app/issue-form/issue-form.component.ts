e21e8de9f036560c727842331e4327e05db8cff2
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-08 13:00:58 +0100

    Fix: prevent default form submission behaviour

--- src/app/issue-form/issue-form.component.ts
+++ src/app/issue-form/issue-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-issue-form',
@@ -7,12 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueFormComponent implements OnInit {
  
+  issueForm = new FormGroup({});
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
+    console.log('called');
   }
 
 }
