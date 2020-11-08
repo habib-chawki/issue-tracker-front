9b62b820c50f3189edc2ae8f4e7c9fca13001a5b
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-08 14:35:53 +0100

    Add meaningful comments

--- src/app/issue-form/issue-form.component.spec.ts
+++ src/app/issue-form/issue-form.component.spec.ts
@@ -29,16 +29,25 @@ describe('IssueFormComponent', () => {
   });
 
   it('should render a form with a text input and a submit button', () => {
+    // the form should be rendered
+    expect(nativeElement.querySelector('form')).toBeTruthy();
+
+    // the description input field shoud be rendered
     expect(nativeElement.querySelector('form input#description')).toBeTruthy();
+
+    // the submit button should be rendered with an add issue label
     expect(nativeElement.querySelector('form button[type="submit"]').innerHTML).toContain('Add issue');
   });
 
-  fit('should invoke onSubmit when form is submitted', () => {
+  it('should invoke onSubmit when form is submitted', () => {
+    // given the onSubmit method
     spyOn(component, "onSubmit").and.callThrough();
 
+    // when the submit button is clicked
     const submitButton: HTMLElement = nativeElement.querySelector('form button[type="submit"]');
     submitButton.click();
 
+    // the method should be invoked
     expect(component.onSubmit).toHaveBeenCalled();
   });
 
