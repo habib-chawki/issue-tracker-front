5a2fef899bdb87d005b724eda2e4fb648f79225d
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-09 13:24:25 +0100

    Test: issue details should be rendered correctly

--- src/app/components/issue/issue.component.spec.ts
+++ src/app/components/issue/issue.component.spec.ts
@@ -28,7 +28,32 @@ describe('IssueComponent', () => {
     expect(nativeElement.querySelector('p').textContent).toEqual('issue');
   });
 
-  fit('should have a details property', () => {
+  fit('should render issue details', () => {
+    // expect details to be empty at first
     expect(component.details).toEqual({ description: '' });
+
+    // given the new details
+    let details = { description: 'This is the issue description' };
+    component.details = details;
+
+    fixture.detectChanges();
+
+    // expect details to be rendered
+    expect(nativeElement.querySelector('p').textContent).toEqual(
+      details.description
+    );
+
+    // given updated details
+    details = { description: 'This is an updated description' };
+
+    // when issue details are updated
+    component.details = details;
+
+    fixture.detectChanges();
+
+    // then expect updated details to be rendered
+    expect(nativeElement.querySelector('p').textContent).toEqual(
+      details.description
+    );
   });
 });
