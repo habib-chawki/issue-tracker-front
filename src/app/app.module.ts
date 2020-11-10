ccb2bcd0a6b497af2bb685b0e937d22f6e8aca21
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-10 22:01:22 +0100

    Rearange imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,7 +8,6 @@ import { AppComponent } from './app.component';
 import { IssueComponent } from './components/issue/issue.component';
 import { IssuesComponent } from './components/issues/issues.component';
 import { IssueFormComponent } from './components/issue-form/issue-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
 import { IssueDetailsComponent } from './components/issue-details/issue-details.component';
 
 @NgModule({
