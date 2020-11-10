a472c243f2635cd61a9562cb43d9b7f358985cba
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-10 22:01:54 +0100

    Initialize details property

--- src/app/components/issue-details/issue-details.component.ts
+++ src/app/components/issue-details/issue-details.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./issue-details.component.scss'],
 })
 export class IssueDetailsComponent implements OnInit {
-  @Input() details: string = '';
+  @Input() details = '';
 
   constructor() {}
 
