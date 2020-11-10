0a51f1b33db66798760641ccfb5e3f187a28de0f
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-10 11:02:35 +0100

    Add issue details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { IssueComponent } from './components/issue/issue.component';
 import { IssuesComponent } from './components/issues/issues.component';
 import { IssueFormComponent } from './components/issue-form/issue-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { IssueDetailsComponent } from './components/issue-details/issue-details.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     IssueComponent,
     IssuesComponent,
     IssueFormComponent,
+    IssueDetailsComponent,
   ],
   imports: [ReactiveFormsModule, BrowserModule, AppRoutingModule],
   providers: [],
