c4c1be5117fc0a8c568cd0146aeda8509f46cfbe
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-10 11:54:16 +0100

    Emit 'issueClicked' event with issue details when 'onClick' is invoked

--- src/app/components/issue/issue.component.ts
+++ src/app/components/issue/issue.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-issue',
@@ -7,10 +7,13 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class IssueComponent implements OnInit {
   @Input() details: object = { description: '' };
+  @Output() issueClicked = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  onClick() {}
+  onClick() {
+    this.issueClicked.emit(this.details);
+  }
 }
