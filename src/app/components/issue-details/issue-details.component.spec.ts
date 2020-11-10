43727a0b171fa51e766dce862ad5c04d280e42db
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-10 13:10:39 +0100

    Test: issue details should be rendered

--- src/app/components/issue-details/issue-details.component.spec.ts
+++ src/app/components/issue-details/issue-details.component.spec.ts
@@ -1,3 +1,4 @@
+import { asNativeElements } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IssueDetailsComponent } from './issue-details.component';
@@ -5,21 +6,29 @@ import { IssueDetailsComponent } from './issue-details.component';
 describe('IssueDetailsComponent', () => {
   let component: IssueDetailsComponent;
   let fixture: ComponentFixture<IssueDetailsComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IssueDetailsComponent ]
-    })
-    .compileComponents();
+      declarations: [IssueDetailsComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IssueDetailsComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should render issue details', () => {
+    expect(nativeElement.querySelector('div').textContent).toEqual(
+      component.details
+    );
   });
 });
