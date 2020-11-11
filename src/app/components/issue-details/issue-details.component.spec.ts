bea26dd0e2c8142ab1e1e72f41d277b0862fefc6
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-11 16:29:03 +0100

    Test: should render all issue details

--- src/app/components/issue-details/issue-details.component.spec.ts
+++ src/app/components/issue-details/issue-details.component.spec.ts
@@ -48,10 +48,24 @@ describe('IssueDetailsComponent', () => {
   });
 
   fit('should render issue details', () => {
-    // expect the rendered details to be bound to the model property
+    // expect the rendered details to be empty at first
     expect(nativeElement.querySelector('div#description').textContent).toEqual(
       ''
     );
+    expect(nativeElement.querySelector('div#summary').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#type').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#status').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#resolution').textContent).toEqual(
+      ''
+    );
+    expect(nativeElement.querySelector('div#assignee').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#reporter').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#comments').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#votes').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#watchers').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#create').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#updated').textContent).toEqual('');
+    expect(nativeElement.querySelector('div#estimate').textContent).toEqual('');
 
     // // when details are updated
     // const details = 'Issue details';
