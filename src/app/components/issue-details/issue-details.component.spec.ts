60512eef9ff40bcaffdef94da3dfdc8f4e16ebf8
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-14 18:27:48 +0100

    Fix: render list of comments test

--- src/app/components/issue-details/issue-details.component.spec.ts
+++ src/app/components/issue-details/issue-details.component.spec.ts
@@ -61,33 +61,42 @@ describe('IssueDetailsComponent', () => {
     expect(nativeElement.querySelector('div#description').textContent).toEqual(
       issue.description
     );
+
     expect(nativeElement.querySelector('div#summary').textContent).toEqual(
       issue.summary
     );
+
     expect(nativeElement.querySelector('div#type').textContent).toEqual(
       issue.type
     );
+
     expect(nativeElement.querySelector('div#status').textContent).toEqual(
       issue.status
     );
+
     expect(nativeElement.querySelector('div#resolution').textContent).toEqual(
       issue.resolution
     );
+
     expect(nativeElement.querySelector('div#assignee').textContent).toEqual(
       issue.assignee
     );
+
     expect(nativeElement.querySelector('div#reporter').textContent).toEqual(
       issue.reporter
     );
 
-    // the list of comments should be rendered
-    nativeElement.querySelectorAll('div#comments ul li').forEach((comment) => {
-      expect(issue.comments).toContain(comment.textContent);
-    });
-
-    expect(nativeElement.querySelector('div#votes').textContent).toEqual(
+    expect(nativeElement.querySelector('div#votes').textContent).toContain(
       issue.votes.toString()
     );
+
+    // the list of comments should be rendered
+    const commentsList = [];
+    nativeElement.querySelectorAll('div#comments ul li').forEach((comment) => {
+      commentsList.push(comment.textContent);
+    });
+
+    expect(commentsList).toEqual(issue.comments);
 
     // the list of watchers should be rendered
     const watchersList = [];
@@ -97,12 +106,15 @@ describe('IssueDetailsComponent', () => {
 
     expect(watchersList).toEqual(issue.watchers);
 
+    // dates should be rendered
     expect(nativeElement.querySelector('div#created').textContent).toContain(
       issue.created.toDateString()
     );
+
     expect(nativeElement.querySelector('div#updated').textContent).toContain(
       issue.updated.toDateString()
     );
+
     expect(nativeElement.querySelector('div#estimate').textContent).toContain(
       issue.estimate.toDateString()
     );
