38d9d9812b2b5667b572bbc2ab8dd984f1f1d4f4
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-17 14:01:55 +0100

    Set up http client and http testing controller

--- src/app/services/issue.service.spec.ts
+++ src/app/services/issue.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpTestingController } from '@angular/common/http/testing';
 
 import { IssueService } from './issue.service';
 
 describe('IssueService', () => {
   let service: IssueService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
