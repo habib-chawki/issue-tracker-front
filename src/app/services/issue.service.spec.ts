f13c1d4d1f4c3e64acedb8f0c2d3e137cd019780
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-17 16:32:54 +0100

    Test: get a list of issues

--- src/app/services/issue.service.spec.ts
+++ src/app/services/issue.service.spec.ts
@@ -1,20 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClient } from '@angular/common/http';
-import { HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 
 import { IssueService } from './issue.service';
+import { Issue } from '../models/issue';
 
 describe('IssueService', () => {
   let service: IssueService;
-  let httpClient: HttpClient;
   let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+
     service = TestBed.inject(IssueService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  fit('should get a list of issues', () => {
+    const mockIssues = [{} as Issue, {} as Issue];
+
+    service.getIssues().subscribe((issues) => {
+      expect(issues).toEqual(mockIssues);
+    });
+
+    // mock http controller
+    const req = httpTestingController.expectOne('http://localhost:80/issues');
+    expect(req.request.method).toBe('GET');
+
+    req.flush(mockIssues);
   });
 });
