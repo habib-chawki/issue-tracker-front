79cfb2fdf650bd2c5917a52471d0ae106327e1d8
habib-chawki <touati.habib.chawki@gmail.com>
2020-11-18 14:56:59 +0100

    Test createIssue: request body should contain the issue object

--- src/app/services/issue.service.spec.ts
+++ src/app/services/issue.service.spec.ts
@@ -14,7 +14,7 @@ describe('IssueService', () => {
   let service: IssueService;
   let httpTestingController: HttpTestingController;
 
-  let issue: Issue;
+  let issue, issue2: Issue;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -26,6 +26,7 @@ describe('IssueService', () => {
   });
 
   beforeEach(() => {
+    // set up an issue
     issue = {
       id: '1',
       key: 'Dh85m',
@@ -43,6 +44,25 @@ describe('IssueService', () => {
       updated: new Date(),
       estimate: new Date(),
     };
+
+    // set up another issue
+    issue2 = {
+      id: '2',
+      key: 'Rt9xP',
+      description: 'Issue 2 description',
+      summary: 'Issue 2 summary',
+      type: IssueType.Story,
+      status: IssueStatus.Todo,
+      resolution: IssueResolution.Unresolved,
+      assignee: 'You',
+      reporter: 'Someone else',
+      comments: ['comment1', 'comment2', 'comment3'],
+      votes: 2,
+      watchers: [],
+      created: new Date(),
+      updated: new Date(),
+      estimate: new Date(),
+    };
   });
 
   afterEach(() => {
@@ -53,25 +73,25 @@ describe('IssueService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('should get a list of issues', () => {
-    // given a list of mocked issues
-    const issues = [{} as Issue, {} as Issue];
-
-    // when the "getIssues()" method is invoked, expect the response to be the list of issues
-    service.getIssues().subscribe((response) => {
-      expect(response).toEqual(issues);
+  it('should create a new issue', () => {
+    // when the "createIssue()" method is invoked with an issue object
+    // then the response should be the issue object itself
+    service.createIssue(issue).subscribe((response) => {
+      expect(response).toEqual(issue);
     });
 
-    // mock http controller and expect a 'GET' request
+    // expect a 'POST' request with the issue object
     const req = httpTestingController.expectOne(service.baseUrl);
-    expect(req.request.method).toBe('GET');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(issue);
 
-    // return the list of mocked issues
-    req.flush(issues);
+    // return the issue back
+    req.flush(issue);
   });
 
   it('should get a single issue by id', () => {
-    // when the "getIssue()" service method is invoked, expect the response to be the issue
+    // when the "getIssue()" service method is invoked with the issue id
+    // then the response should be the issue with the correct id
     service.getIssue('1').subscribe((response) => {
       expect(response).toEqual(issue);
     });
@@ -84,16 +104,20 @@ describe('IssueService', () => {
     req.flush(issue);
   });
 
-  fit('should create a new issue', () => {
-    service.createIssue(issue).subscribe((response) => {
-      expect(response).toEqual(issue);
+  it('should get a list of issues', () => {
+    // given a list of issues
+    const issues = [issue, issue2];
+
+    // when the "getIssues()" method is invoked, expect the response to be the list of issues
+    service.getIssues().subscribe((response) => {
+      expect(response).toEqual(issues);
     });
 
-    // expect a 'POST' request with the issue
+    // expect a 'GET' request
     const req = httpTestingController.expectOne(service.baseUrl);
-    expect(req.request.method).toBe('POST');
+    expect(req.request.method).toBe('GET');
 
-    // return the issue back
-    req.flush(issue);
+    // return the list of mocked issues
+    req.flush(issues);
   });
 });
