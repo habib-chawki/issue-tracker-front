52b42948d2b95926b581448293c26bb6e81af20b
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-17 16:03:14 +0100

    Test: user signup

--- src/app/services/user/user.service.spec.ts
+++ src/app/services/user/user.service.spec.ts
@@ -1,16 +1,52 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
+  let httpTestingController: HttpTestingController;
+
+  let userDetails;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
+    userService = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  beforeEach(() => {
+    userDetails = {
+      firstName: 'someone',
+      lastName: 'something',
+      username: 'one.thing',
+      email: 'stephen.king@email.com',
+      password: 'Redman',
+    };
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  fit('should create the user', () => {
+    userService.signUp(userDetails).subscribe((response) => {
+      expect(response).toBe(userDetails);
+    });
+
+    const req = httpTestingController.expectOne(userService.baseUrl);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toBe(userDetails);
+
+    req.flush(userDetails);
   });
 });
