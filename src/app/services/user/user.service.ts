c76ed69b3d7fffa554cf0544b8d9a49e8434a657
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-17 16:03:26 +0100

    Handle user signup

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,10 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  constructor() {}
+  baseUrl = '/users/signup';
 
-  signUp(userDetails) {}
+  constructor(private httpClient: HttpClient) {}
+
+  signUp(userDetails): Observable<any> {
+    return this.httpClient.post(this.baseUrl, userDetails);
+  }
 }
