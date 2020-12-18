50014d3ac1690779baf2e0feb4404180e4aa81b1
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-18 13:47:07 +0100

    Test: a login form should be rendered

--- src/app/components/login-form/login-form.component.spec.ts
+++ src/app/components/login-form/login-form.component.spec.ts
@@ -5,21 +5,27 @@ import { LoginFormComponent } from './login-form.component';
 describe('LoginFormComponent', () => {
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
-    })
-    .compileComponents();
+      declarations: [LoginFormComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should render login form with email and password input fields', () => {
+    expect(nativeElement.querySelector('form')).toBeTruthy();
   });
 });
