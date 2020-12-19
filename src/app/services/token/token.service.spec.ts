188a3fd2ea93a519d4ae98e6749a376eedaae1fb
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-19 13:10:53 +0100

    Clear localStorage after each test

--- src/app/services/token/token.service.spec.ts
+++ src/app/services/token/token.service.spec.ts
@@ -1,17 +1,22 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TokenService } from './token.service';
+import { StorageService } from './token.service';
 
 describe('TokenService', () => {
-  let tokenService: TokenService;
+  let storageService: StorageService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    tokenService = TestBed.inject(TokenService);
+    storageService = TestBed.inject(StorageService);
+  });
+
+  afterEach(() => {
+    // clear localStorage after each test
+    localStorage.clear();
   });
 
   it('should be created', () => {
-    expect(tokenService).toBeTruthy();
+    expect(storageService).toBeTruthy();
   });
 
   it('should store the token in localStorage', () => {
@@ -21,7 +26,7 @@ describe('TokenService', () => {
     const token = prefix + body;
 
     // when storeToken() is called
-    tokenService.storeToken(token);
+    storageService.storeToken(token);
 
     // then the token should be stored in localStorage
     expect(localStorage.getItem('token')).toBe(body);
@@ -29,7 +34,7 @@ describe('TokenService', () => {
 
   it('should determine whether the user is logged in or not', () => {
     // when auth token is not present then the user is not logged in
-    expect(tokenService.isUserLoggedIn()).toBeFalse();
+    expect(storageService.isUserLoggedIn()).toBeFalse();
 
     const token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuX2VtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMn0.gDUCVU7FRVzGRXU3VTK45Q5h7of7HNwLlYtrPz1MrJU';
@@ -38,16 +43,16 @@ describe('TokenService', () => {
     localStorage.setItem('token', token);
 
     // when the auth token is present then the user is logged in
-    expect(tokenService.isUserLoggedIn()).toBeTrue();
+    expect(storageService.isUserLoggedIn()).toBeTrue();
   });
 
-  fit('should store user identifier', () => {
+  it('should store user identifier', () => {
     const userIdentifier = 'harper.lee@email.com';
 
     // user identifier should not be present in localStorage at first
     expect(localStorage.getItem('userIdentifier')).toBeNull();
 
-    tokenService.storeUserIdentifier(userIdentifier);
+    storageService.storeUserIdentifier(userIdentifier);
 
     // expect the user identifier to have been stored successfully
     expect(localStorage.getItem('identifier')).toBe(userIdentifier);
