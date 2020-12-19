b052e3a2f6d515e00ef8c39e7e35875f4113143f
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-19 12:01:29 +0100

    Test whether user is logged in or not

--- src/app/services/token/token.service.spec.ts
+++ src/app/services/token/token.service.spec.ts
@@ -16,7 +16,8 @@ describe('TokenService', () => {
 
   it('should store the token in localStorage', () => {
     const prefix = 'Bearer ';
-    const body = 'GdRhx85$.hTedRU9@.LeSXi8Y5G33';
+    const body =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im15X2VtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMn0.cEHP5JqC-0APiSyCltuSAMmCjQqJFw9YfiHjOBjZPmI';
     const token = prefix + body;
 
     // when storeToken() is called
@@ -24,5 +25,19 @@ describe('TokenService', () => {
 
     // then the token should be stored in localStorage
     expect(localStorage.getItem('token')).toBe(body);
+  });
+
+  fit('should determine whether the user is logged in or not', () => {
+    // when auth token is not present then the user is not logged in
+    expect(tokenService.isUserLoggedIn()).toBeFalse();
+
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuX2VtYWlsLmNvbSIsImlhdCI6MTUxNjIzOTAyMn0.gDUCVU7FRVzGRXU3VTK45Q5h7of7HNwLlYtrPz1MrJU';
+
+    // set the auth token
+    localStorage.setItem('token', token);
+
+    // when the auth token is present then the user is logged in
+    expect(tokenService.isUserLoggedIn()).toBeTrue();
   });
 });
