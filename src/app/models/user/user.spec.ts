16da133bee90224ff6e88216440005fe158ac311
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-22 16:56:13 +0100

    Test: user model

--- src/app/models/user/user.spec.ts
+++ src/app/models/user/user.spec.ts
@@ -2,6 +2,6 @@ import { User } from './user';
 
 describe('User', () => {
   it('should create an instance', () => {
-    expect(new User('some.user@email.com')).toBeTruthy();
+    expect(new User()).toBeTruthy();
   });
 });
