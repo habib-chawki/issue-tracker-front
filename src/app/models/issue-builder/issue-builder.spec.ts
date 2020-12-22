ba0f17578c6484a2d28ad9f80a8cbe70550be3c7
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-22 20:45:44 +0100

    Test: create issue with IssueBuilder

--- src/app/models/issue-builder/issue-builder.spec.ts
+++ src/app/models/issue-builder/issue-builder.spec.ts
@@ -6,44 +6,46 @@ import { IssueBuilder } from './issue-builder';
 
 let issue: Issue;
 
-beforeEach(() => {
-  issue = {
-    id: '500',
-    key: 'SoldkeMP5sdfE',
-    description: 'Desc',
-    summary: 'Summary',
-    type: IssueType.Story,
-    status: IssueStatus.InProgress,
-    resolution: IssueResolution.Unresolved,
-    assignee: null,
-    reporter: null,
-    comments: [],
-    votes: 0,
-    watchers: [],
-    creationTime: new Date(),
-    updateTime: new Date(),
-    estimate: new Date(),
-  };
-});
+describe('IssueBuilder', () => {
+  beforeEach(() => {
+    issue = {
+      id: '500',
+      key: 'SoldkeMP5sdfE',
+      description: 'Desc',
+      summary: 'Summary',
+      type: IssueType.Story,
+      status: IssueStatus.InProgress,
+      resolution: IssueResolution.Unresolved,
+      assignee: null,
+      reporter: null,
+      comments: [],
+      votes: 0,
+      watchers: [],
+      creationTime: new Date(),
+      updateTime: new Date(),
+      estimate: new Date(),
+    };
+  });
 
-it('should create an issue', () => {
-  const createdIssue: Issue = new IssueBuilder()
-    .id(issue.id)
-    .key(issue.key)
-    .description(issue.description)
-    .summary(issue.summary)
-    .type(issue.type)
-    .status(issue.status)
-    .resolution(issue.resolution)
-    .assignee(issue.assignee)
-    .reporter(issue.reporter)
-    .comments(issue.comments)
-    .votes(issue.votes)
-    .watchers(issue.watchers)
-    .creationTime(issue.creationTime)
-    .updateTime(issue.updateTime)
-    .estimate(issue.estimate)
-    .build();
+  it('should create an issue', () => {
+    const createdIssue: Issue = new IssueBuilder()
+      .id(issue.id)
+      .key(issue.key)
+      .description(issue.description)
+      .summary(issue.summary)
+      .type(issue.type)
+      .status(issue.status)
+      .resolution(issue.resolution)
+      .assignee(issue.assignee)
+      .reporter(issue.reporter)
+      .comments(issue.comments)
+      .votes(issue.votes)
+      .watchers(issue.watchers)
+      .creationTime(issue.creationTime)
+      .updateTime(issue.updateTime)
+      .estimate(issue.estimate)
+      .build();
 
-  expect(createdIssue).toBe(issue);
+    expect(createdIssue).toEqual(issue);
+  });
 });
