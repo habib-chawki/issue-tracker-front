ce1342894ac5f8fb0dee6147f472aa644fb8353b
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-22 16:34:45 +0100

    Add issue builder

---
+++ src/app/models/issue-builder/issue-builder.ts
@@ -0,0 +1,105 @@
+import IssueResolution from '../enums/issue-resolution';
+import IssueStatus from '../enums/issue-status';
+import IssueType from '../enums/issue-type';
+import { Issue } from '../issue/issue';
+import { User } from '../user/user';
+
+export class IssueBuilder {
+  private readonly _issue: Issue;
+
+  constructor() {
+    this._issue = {
+      id: '',
+      key: '',
+      description: '',
+      summary: '',
+      type: null,
+      status: null,
+      resolution: null,
+      assignee: null,
+      reporter: null,
+      comments: [],
+      votes: 0,
+      watchers: [],
+      creationTime: new Date(),
+      updateTime: new Date(),
+      estimate: new Date(),
+    };
+  }
+
+  id(id: string): IssueBuilder {
+    this._issue.id = id;
+    return this;
+  }
+
+  key(key: string): IssueBuilder {
+    this._issue.key = key;
+    return this;
+  }
+
+  description(description: string): IssueBuilder {
+    this._issue.description = description;
+    return this;
+  }
+
+  summary(summary: string): IssueBuilder {
+    this._issue.summary = summary;
+    return this;
+  }
+
+  type(type: IssueType): IssueBuilder {
+    this._issue.type = type;
+    return this;
+  }
+
+  status(status: IssueStatus): IssueBuilder {
+    this._issue.status = status;
+    return this;
+  }
+
+  resolution(resolution: IssueResolution): IssueBuilder {
+    this._issue.resolution = resolution;
+    return this;
+  }
+
+  assignee(assignee: User): IssueBuilder {
+    this._issue.assignee = assignee;
+    return this;
+  }
+  reporter(reporter: User): IssueBuilder {
+    this._issue.reporter = reporter;
+    return this;
+  }
+  comments(comments: string[]): IssueBuilder {
+    this._issue.comments = comments;
+    return this;
+  }
+  votes(votes: number): IssueBuilder {
+    this._issue.votes = votes;
+    return this;
+  }
+
+  watchers(watchers: string[]): IssueBuilder {
+    this._issue.watchers = watchers;
+    return this;
+  }
+
+  creationTime(creationTime: Date): IssueBuilder {
+    this._issue.creationTime = creationTime;
+    return this;
+  }
+
+  updateTime(updateTime: Date): IssueBuilder {
+    this._issue.updateTime = updateTime;
+    return this;
+  }
+
+  estimate(estimate: Date): IssueBuilder {
+    this._issue.estimate = estimate;
+    return this;
+  }
+
+  build(): Issue {
+    return this._issue;
+  }
+}
