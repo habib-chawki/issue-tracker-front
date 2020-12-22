d4b64946a7f0008d7f2ec2ddcdf844d2932d49de
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-22 16:56:27 +0100

    Add user model

--- src/app/models/user/user.ts
+++ src/app/models/user/user.ts
@@ -1,3 +1,31 @@
 export class User {
-  constructor(private _email: string) {}
+  private _id: string = '1';
+  private _username: string = 'Jon Doe';
+  private _email: string = 'jon.doe@email.com';
+
+  constructor() {}
+
+  public get id() {
+    return this._id;
+  }
+
+  public set id(id) {
+    this._id = id;
+  }
+
+  public get username() {
+    return this._username;
+  }
+
+  public set username(username) {
+    this._username = username;
+  }
+
+  public get email() {
+    return this._email;
+  }
+
+  public set email(email) {
+    this._email = email;
+  }
 }
