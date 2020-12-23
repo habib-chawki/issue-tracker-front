e2c716f1b9375e7b18eaf9c3a3fa4ce4eb4298ae
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-23 15:02:12 +0100

    Add comment property

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Comment } from 'src/app/models/comment/comment';
 
 @Component({
   selector: 'app-comment',
   templateUrl: './comment.component.html',
-  styleUrls: ['./comment.component.scss']
+  styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent implements OnInit {
+  comment: Comment;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
