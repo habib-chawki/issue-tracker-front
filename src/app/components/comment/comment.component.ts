0e74fe469a9cfdb00c06c3a4c4d5097cef705c97
habib-chawki <touati.habib.chawki@gmail.com>
2020-12-23 16:22:27 +0100

    Fix: add @Input() annotation to comment property

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Comment } from 'src/app/models/comment/comment';
 
 @Component({
@@ -7,7 +7,7 @@ import { Comment } from 'src/app/models/comment/comment';
   styleUrls: ['./comment.component.scss'],
 })
 export class CommentComponent implements OnInit {
-  comment: Comment;
+  @Input() comment: Comment;
 
   constructor() {}
 
