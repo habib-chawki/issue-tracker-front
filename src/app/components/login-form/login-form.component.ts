330504fc1c1318cebe540af0c2974e4827b18f32
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-03 12:33:41 +0100

    Validate emails

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { StorageService } from 'src/app/services/storage/storage.service';
 import { UserService } from 'src/app/services/user/user.service';
 
@@ -10,7 +10,7 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class LoginFormComponent implements OnInit {
   loginForm = new FormGroup({
-    email: new FormControl(''),
+    email: new FormControl('', Validators.email),
     password: new FormControl(''),
   });
 
