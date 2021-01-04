15a46d38244e1e4f3b3d4636d3ec5a5c7e937e35
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-04 16:37:44 +0100

    Add login route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Backlog } from './components/backlog/backlog.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 export const routes: Routes = [
   { path: '', component: Backlog },
   { path: 'signup', component: SignupFormComponent },
+  { path: 'login', component: LoginFormComponent },
 ];
 
 @NgModule({
