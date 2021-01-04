73469fb80f2028582a43f7029dd29efbd53e4597
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-04 16:39:22 +0100

    Add backlog route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginFormComponent } from './components/login-form/login-form.component
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 export const routes: Routes = [
-  { path: '', component: Backlog },
+  { path: 'backlog', component: Backlog },
   { path: 'signup', component: SignupFormComponent },
   { path: 'login', component: LoginFormComponent },
 ];
