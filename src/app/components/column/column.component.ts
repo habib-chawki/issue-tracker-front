a5b7e396c80db8a8a405274893f7055aa9a0cc44
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-12 11:34:07 +0100

    Render column title

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
-  styleUrls: ['./column.component.scss']
+  styleUrls: ['./column.component.scss'],
 })
 export class ColumnComponent implements OnInit {
+  title: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
