7c2fabb609e1811b1975ae8d7ab1bd821c69d977
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-12 11:55:48 +0100

    Add list of issues to column component

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from 'src/app/models/issue/issue';
 
 @Component({
   selector: 'app-column',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColumnComponent implements OnInit {
   title: string;
+  issues: Issue[];
 
   constructor() {}
 
