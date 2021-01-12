19b62fc1c407a98cf5b93e559f05142cc622e583
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-12 11:33:54 +0100

    Test: render column title

--- src/app/components/column/column.component.spec.ts
+++ src/app/components/column/column.component.spec.ts
@@ -5,21 +5,34 @@ import { ColumnComponent } from './column.component';
 describe('ColumnComponent', () => {
   let component: ColumnComponent;
   let fixture: ComponentFixture<ColumnComponent>;
+  let nativeElement: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ColumnComponent ]
-    })
-    .compileComponents();
+      declarations: [ColumnComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ColumnComponent);
     component = fixture.componentInstance;
+    nativeElement = fixture.nativeElement;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fit('should render a title', () => {
+    // given a title
+    component.title = 'Column title';
+
+    fixture.detectChanges();
+
+    expect(nativeElement.querySelector('div#title').textContent).toBe(
+      component.title
+    );
   });
 });
