43310d23e15bc79beff8e11e270d1093f6cd4129
habib-chawki <touati.habib.chawki@gmail.com>
2021-01-12 12:45:52 +0100

    Add list of columns property

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from 'src/app/models/issue/issue';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss'],
 })
 export class BoardComponent implements OnInit {
+  columns: { title: string; issues: Issue[] }[];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
