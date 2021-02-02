7d4c0b9ad4d8af83c4ec0b36bba9828cb3ae2fab
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-02 15:12:51 +0100

    Handle save button click

--- src/app/components/column-form/column-form.component.ts
+++ src/app/components/column-form/column-form.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-column-form',
   templateUrl: './column-form.component.html',
-  styleUrls: ['./column-form.component.scss']
+  styleUrls: ['./column-form.component.scss'],
 })
 export class ColumnFormComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  onSave() {}
 }
