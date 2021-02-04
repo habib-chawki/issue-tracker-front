951c2e1d2707b810ea511c228ae4294a5b5f6564
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-04 14:06:26 +0100

    add POST request: handle create column

--- src/app/services/column/column.service.ts
+++ src/app/services/column/column.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import Column from 'src/app/models/column/column';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ColumnService {
+  baseUrl = `http://localhost:80/boards/boardId/columns`;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  createColumn(column: Column, boardId: string): Observable<Column> {
+    return this.httpClient.post<Column>(
+      this.baseUrl.replace('boardId', boardId),
+      column
+    );
+  }
 }
