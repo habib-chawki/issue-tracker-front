43cb548b7647974948bbd58adae6c118045e99e8
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-04 14:00:22 +0100

    Set up column service test suite

--- src/app/services/column/column.service.spec.ts
+++ src/app/services/column/column.service.spec.ts
@@ -3,21 +3,40 @@ import {
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import Column from 'src/app/models/column/column';
 
 import { ColumnService } from './column.service';
 
 describe('ColumnService', () => {
   let service: ColumnService;
-  let httpClient: HttpTestingController;
+  let httpTestingController: HttpTestingController;
+
+  const boardId = '100';
+  const baseUrl = `http://localhost:80/boards/${boardId}/columns`;
+
+  let column: Column;
 
   beforeEach(() => {
     TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
 
     service = TestBed.inject(ColumnService);
-    httpClient = TestBed.inject(HttpTestingController);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  beforeEach(() => {
+    column = {
+      id: '10',
+      title: 'To do',
+    };
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should create column', () => {});
 });
