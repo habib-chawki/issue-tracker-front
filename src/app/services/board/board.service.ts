6e9983f0726e6bf60fd1274bc2034df8378cf2dc
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-04 13:38:38 +0100

    Handle create board

--- src/app/services/board/board.service.ts
+++ src/app/services/board/board.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import Board from 'src/app/models/board/board';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BoardService {
+  baseUrl = 'localhost:80/boards';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  createBoard(board: Board): Observable<Board> {
+    return this.httpClient.post<Board>(this.baseUrl, board);
+  }
 }
