d87a2105b4bcb3fc2e9be727ae361bec807de146
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-04 12:35:11 +0100

    Add board service

--- src/app/services/column/column.service.spec.ts
+++ src/app/services/column/column.service.spec.ts
@@ -1,13 +1,20 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ColumnService } from './column.service';
 
 describe('ColumnService', () => {
   let service: ColumnService;
+  let httpClient: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+
     service = TestBed.inject(ColumnService);
+    httpClient = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
