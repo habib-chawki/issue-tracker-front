b93557d8c6003385a39c900e9c5f3214abcde9ad
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-04 12:49:41 +0100

    Set up board service test suite

--- src/app/services/board/board.service.spec.ts
+++ src/app/services/board/board.service.spec.ts
@@ -1,16 +1,35 @@
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { BoardService } from './board.service';
 
 describe('BoardService', () => {
-  let service: BoardService;
+  let boardService: BoardService;
+  let httpTestingController: HttpTestingController;
+
+  let board: Board;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(BoardService);
+    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+
+    boardService = TestBed.inject(BoardService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  beforeEach(() => {});
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(boardService).toBeTruthy();
+  });
+
+  it('should create board', () => {
+    boardService.createBoard().subscribe((board: Board) => {});
   });
 });
