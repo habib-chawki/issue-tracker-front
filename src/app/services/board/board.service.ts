8bbc5dade24339818c494e1e655b2ef64ef2a79c
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-04 13:50:45 +0100

    Handle get board by id

--- src/app/services/board/board.service.ts
+++ src/app/services/board/board.service.ts
@@ -7,11 +7,15 @@ import Board from 'src/app/models/board/board';
   providedIn: 'root',
 })
 export class BoardService {
-  baseUrl = 'localhost:80/boards';
+  baseUrl = 'http://localhost:80/boards';
 
   constructor(private httpClient: HttpClient) {}
 
   createBoard(board: Board): Observable<Board> {
     return this.httpClient.post<Board>(this.baseUrl, board);
+  }
+
+  getBoard(boardId): Observable<Board> {
+    return this.httpClient.get<Board>(`${this.baseUrl}/${boardId}`);
   }
 }
