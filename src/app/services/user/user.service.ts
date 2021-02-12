0f671e27dd29f30dbfb834cc590d807af9bb1782
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-12 16:46:27 +0100

    Handle user signup and login

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,25 +1,25 @@
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  signupUrl = '/users/signup';
-  loginUrl = '/login';
+  signupUrl = 'users/signup';
+  loginUrl = 'login';
 
   constructor(private httpClient: HttpClient) {}
 
   signUp(userCredentials): Observable<HttpResponse<any>> {
-    return this.httpClient.post(this.signupUrl, userCredentials, {
-      observe: 'response',
-    });
+    const url = `${environment.apiUrl}/${this.signupUrl}`;
+
+    return this.httpClient.post(url, userCredentials, { observe: 'response' });
   }
 
   login(userCredentials): Observable<HttpResponse<any>> {
-    return this.httpClient.post(this.loginUrl, userCredentials, {
-      observe: 'response',
-    });
+    const url = `${environment.apiUrl}/${this.loginUrl}`;
+    return this.httpClient.post(url, userCredentials, { observe: 'response' });
   }
 }
