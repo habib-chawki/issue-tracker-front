5f59deef5fa170acd8fd2812223eafb9f5833e4c
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-16 15:07:20 +0100

    Handle remove issues

--- src/app/components/issues/issues.component.ts
+++ src/app/components/issues/issues.component.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit } from '@angular/core';
 import { Issue } from 'src/app/models/issue/issue';
+import { IssueService } from 'src/app/services/issue/issue.service';
 
 @Component({
   selector: 'app-issues',
@@ -14,15 +15,16 @@ import { Issue } from 'src/app/models/issue/issue';
 export class IssuesComponent implements OnInit {
   @Input() issues: Issue[] = [];
 
-  constructor() {}
+  constructor(private issueService: IssueService) {}
 
   ngOnInit(): void {}
 
   // invoked when the issue remove button is clicked
   onRemoveIssue(issue: Issue) {
-    // remove the issue
-    const index = this.issues.indexOf(issue);
-    this.issues.splice(index, 1);
+    this.issueService.deleteIssue(issue.id).subscribe(() => {
+      const index = this.issues.indexOf(issue);
+      this.issues.splice(index, 1);
+    });
   }
 
   // handle issue drop
