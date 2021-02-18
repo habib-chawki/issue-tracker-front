f1abab38d0d54dcf05cb53528cdb8dd0e82c915d
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 20:40:42 +0100

    Listen for form saved announcements

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ProjectFormComponent } from 'src/app/forms/project-form/project-form.component';
 import Project from 'src/app/models/project/project.spec';
+import { ProjectCommunicationService } from 'src/app/services/project-communication/project-communication.service';
 import { ProjectService } from 'src/app/services/project/project.service';
 
 @Component({
@@ -14,6 +15,7 @@ export class ProjectsComponent implements OnInit {
 
   constructor(
     private projectService: ProjectService,
+    private projectCommunicationService: ProjectCommunicationService,
     private dialog: MatDialog
   ) {}
 
@@ -21,6 +23,13 @@ export class ProjectsComponent implements OnInit {
     this.projectService.getProjects().subscribe((response: Project[]) => {
       this.projects = response;
     });
+
+    // listen for project form saved announcements
+    this.projectCommunicationService.projectFormSaved$.subscribe(
+      (project: Project) => {
+        this.projects.push(project);
+      }
+    );
   }
 
   onAddProject() {
