b11403388dfc9972c7104d527ca71024e2af2df2
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 11:43:06 +0100

    Render list of projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import Project from 'src/app/models/project/project.spec';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
+  projects: Project[] = [
+    {
+      id: '1',
+      name: 'project 1',
+    },
+    {
+      id: '2',
+      name: 'project 2',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
