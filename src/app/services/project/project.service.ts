c728efa5b42a5beb4423ef29551e2aa2b00ef3d7
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 11:48:13 +0100

    Fetch list of projects

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -1,8 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import Project from 'src/app/models/project/project.spec';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProjectService {
-  constructor() {}
+  baseUrl = `${environment.apiUrl}/projects`;
+
+  constructor(private httpClient: HttpClient) {}
+
+  getProjects(): Observable<Project[]> {
+    return this.httpClient.get<Project[]>(this.baseUrl);
+  }
 }
