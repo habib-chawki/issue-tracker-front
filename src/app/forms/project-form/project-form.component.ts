d31758fbacdc31cdb96d2265f8cf3007a84ba9bc
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 14:37:32 +0100

    Add project form group

--- src/app/forms/project-form/project-form.component.ts
+++ src/app/forms/project-form/project-form.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-project-form',
   templateUrl: './project-form.component.html',
-  styleUrls: ['./project-form.component.scss']
+  styleUrls: ['./project-form.component.scss'],
 })
 export class ProjectFormComponent implements OnInit {
+  projectForm: FormGroup = new FormGroup({
+    name: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onSave() {}
 }
