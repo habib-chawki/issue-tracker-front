c87ac4df8461dd0755d4949f4392f5184c1af8a5
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 20:14:09 +0100

    Get project backlog

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Issue } from 'src/app/models/issue/issue';
 import Project from 'src/app/models/project/project.spec';
 import { environment } from 'src/environments/environment';
 
@@ -14,5 +15,9 @@ export class ProjectService {
 
   getProjects(): Observable<Project[]> {
     return this.httpClient.get<Project[]>(this.baseUrl);
+  }
+
+  getBacklog(projectId: string): Observable<Issue[]> {
+    return this.httpClient.get<Issue[]>(`${this.baseUrl}/${projectId}/issues`);
   }
 }
