69bc144de33c8e02f095c2c90a9c6b2307d4f355
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 20:28:47 +0100

    Announce project form saved

--- src/app/services/project-communication/project-communication.service.ts
+++ src/app/services/project-communication/project-communication.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import Project from 'src/app/models/project/project.spec';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectCommunicationService {
+  projectFormSaved$: Subject<Project>;
 
-  constructor() { }
+  constructor() {}
+
+  announceProjectFormSaved(project: Project) {
+    this.projectFormSaved$.next(project);
+  }
 }
