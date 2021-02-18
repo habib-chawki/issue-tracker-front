281b81f04e5103c57068b7fd5ff90494d6966f6f
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 11:43:37 +0100

    Add projects route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { BacklogComponent } from './components/backlog/backlog.component';
 import { BoardComponent } from './components/board/board.component';
 import { LoginFormComponent } from './components/login-form/login-form.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 import { SignupFormComponent } from './components/signup-form/signup-form.component';
 
 export const routes: Routes = [
   { path: 'signup', component: SignupFormComponent },
   { path: 'login', component: LoginFormComponent },
+  { path: 'projects', component: ProjectsComponent },
   { path: 'backlog', component: BacklogComponent },
   { path: 'board', component: BoardComponent },
   { path: '', redirectTo: 'signup', pathMatch: 'full' },
