e13d3b76443c46b5889c07828a8e7039186d42e6
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-18 14:26:06 +0100

    Handle add new project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import Project from 'src/app/models/project/project.spec';
 import { ProjectService } from 'src/app/services/project/project.service';
 
@@ -10,11 +11,16 @@ import { ProjectService } from 'src/app/services/project/project.service';
 export class ProjectsComponent implements OnInit {
   projects: Project[] = [];
 
-  constructor(private projectService: ProjectService) {}
+  constructor(
+    private projectService: ProjectService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.projectService.getProjects().subscribe((response: Project[]) => {
       this.projects = response;
     });
   }
+
+  onAddProject() {}
 }
