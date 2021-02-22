b23c7c278d88d17041680052b830cbea83274b24
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-22 12:33:05 +0100

    Add sprint form

--- src/app/forms/sprint-form/sprint-form.component.ts
+++ src/app/forms/sprint-form/sprint-form.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sprint-form',
   templateUrl: './sprint-form.component.html',
-  styleUrls: ['./sprint-form.component.scss']
+  styleUrls: ['./sprint-form.component.scss'],
 })
 export class SprintFormComponent implements OnInit {
+  sprintForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    goal: new FormControl('', Validators.required),
+    startDate: new FormControl(Date.now, Validators.required),
+    endDate: new FormControl(new Date().getDate() + 30, Validators.required),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
