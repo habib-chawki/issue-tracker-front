4d7d2c0b2d5a3cb226db4b98a5d75b892e2e454c
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-25 13:37:39 +0100

    Create sprint

--- src/app/services/sprint/sprint.service.ts
+++ src/app/services/sprint/sprint.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import Sprint from 'src/app/models/sprint/sprint';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SprintService {
+  baseUrl = `${environment.apiUrl}/projects`;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  createSprint(projectId, sprint): Observable<Sprint> {
+    const url = `${this.baseUrl}/${projectId}/sprints`;
+    return this.httpClient.post<Sprint>(url, sprint);
+  }
 }
