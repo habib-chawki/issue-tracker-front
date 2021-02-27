08037405d3f8e24dc93a0b711be6668d664dae21
habib-chawki <touati.habib.chawki@gmail.com>
2021-02-27 12:56:18 +0100

    Add sprint backlog

--- src/app/models/sprint/sprint.ts
+++ src/app/models/sprint/sprint.ts
@@ -1,8 +1,11 @@
+import { Issue } from '../issue/issue';
+
 export default interface Sprint {
   id: string;
   name: string;
   goal: string;
   project: string;
+  backlog: Issue[];
   startDate: Date;
   endDate: Date;
 }
