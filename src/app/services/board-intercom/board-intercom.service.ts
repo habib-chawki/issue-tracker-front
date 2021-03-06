bada950d5251f50f6d299861e291e18facdfaa15
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-06 15:37:28 +0100

    Announce board form saved

--- src/app/services/board-intercom/board-intercom.service.ts
+++ src/app/services/board-intercom/board-intercom.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import Board from 'src/app/models/board/board';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BoardIntercomService {
+  boardFormSaved$ = new Subject<Board>();
 
-  constructor() { }
+  constructor() {}
+
+  annouceBoardFormSaved(board: Board) {
+    this.boardFormSaved$.next(board);
+  }
 }
