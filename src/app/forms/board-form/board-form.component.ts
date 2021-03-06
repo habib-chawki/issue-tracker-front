8cf9fc13bd8565bb9891f0a676d978757e3f62d1
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-06 15:28:23 +0100

    Set board form group

--- src/app/forms/board-form/board-form.component.ts
+++ src/app/forms/board-form/board-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-board-form',
   templateUrl: './board-form.component.html',
-  styleUrls: ['./board-form.component.scss']
+  styleUrls: ['./board-form.component.scss'],
 })
 export class BoardFormComponent implements OnInit {
+  boardForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
