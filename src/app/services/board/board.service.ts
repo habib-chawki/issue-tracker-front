627bd0ffae2ed61febdda21f13dbd7af1fc6ab32
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-07 09:54:04 +0100

    Create board, set sprint param

--- src/app/services/board/board.service.ts
+++ src/app/services/board/board.service.ts
@@ -2,17 +2,22 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import Board from 'src/app/models/board/board';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BoardService {
-  baseUrl = 'http://localhost:80/boards';
+  baseUrl = `${environment.apiUrl}/boards`;
 
   constructor(private httpClient: HttpClient) {}
 
-  createBoard(board: Board): Observable<Board> {
-    return this.httpClient.post<Board>(this.baseUrl, board);
+  createBoard(sprintId: string, board: Board): Observable<Board> {
+    return this.httpClient.post<Board>(this.baseUrl, board, {
+      params: {
+        sprint: sprintId,
+      },
+    });
   }
 
   getBoard(boardId): Observable<Board> {
