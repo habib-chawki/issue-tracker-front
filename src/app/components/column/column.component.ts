1ef179c5ff4a8d5d771866803f22fb37957c460d
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-10 12:02:10 +0100

    Fix: update issue column

--- src/app/components/column/column.component.ts
+++ src/app/components/column/column.component.ts
@@ -5,7 +5,6 @@ import {
 } from '@angular/cdk/drag-drop';
 import { Component, Input, OnInit } from '@angular/core';
 import Column from 'src/app/models/column/column';
-import { Issue } from 'src/app/models/issue/issue';
 import { ColumnService } from 'src/app/services/column/column.service';
 
 @Component({
@@ -22,31 +21,34 @@ export class ColumnComponent implements OnInit {
 
   ngOnInit(): void {}
 
-  onDrop = (event: CdkDragDrop<Issue[]>) => {
+  onDrop = (event: CdkDragDrop<Column>) => {
     if (event.previousContainer === event.container) {
       // drag issues within the same column
       moveItemInArray(
-        event.container.data,
+        event.container.data.issues,
         event.previousIndex,
         event.currentIndex
       );
     } else {
+      const boardId = this.boardId;
+      const columnId = event.previousContainer.data.id;
+      const issueId = event.item.data.id;
+      const newColumnId = event.container.data.id;
+
       // update the issue column
       this.columnService
-        .updateIssueColumn(
-          this.boardId,
-          event.previousContainer.id,
-          event.item.data.id,
-          event.container.id
-        )
-        .subscribe(() => {
-          // drag issues between columns
-          transferArrayItem(
-            event.previousContainer.data,
-            event.container.data,
-            event.previousIndex,
-            event.currentIndex
-          );
+        .updateIssueColumn(boardId, columnId, issueId, newColumnId)
+        .subscribe({
+          next: () => {
+            // drag issues between columns
+            transferArrayItem(
+              event.previousContainer.data.issues,
+              event.container.data.issues,
+              event.previousIndex,
+              event.currentIndex
+            );
+          },
+          error: (error) => console.log('ERROR: ' + JSON.stringify(error)),
         });
     }
   };
