36fdfa282f12a299c0a3438df4ef2bf3cdfc5b5a
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-14 13:05:56 +0100

    Handle display sprint details

--- src/app/components/sprint-details/sprint-details.component.ts
+++ src/app/components/sprint-details/sprint-details.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Sprint from 'src/app/models/sprint/sprint';
 
 @Component({
   selector: 'app-sprint-details',
   templateUrl: './sprint-details.component.html',
-  styleUrls: ['./sprint-details.component.scss']
+  styleUrls: ['./sprint-details.component.scss'],
 })
 export class SprintDetailsComponent implements OnInit {
+  @Input() sprint: Sprint;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onDisplaySprintDetails = () => {
+    console.log(this.sprint);
+  };
 }
