296ec3651654f3e5fde2a9b8b7cc2958350f15ee
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-14 12:12:45 +0100

    Extract project id query param

--- src/app/components/sprints/sprints.component.ts
+++ src/app/components/sprints/sprints.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import SprintStatus from 'src/app/models/enums/sprint-status';
+import { SprintService } from 'src/app/services/sprint/sprint.service';
 
 @Component({
   selector: 'app-sprints',
   templateUrl: './sprints.component.html',
-  styleUrls: ['./sprints.component.scss']
+  styleUrls: ['./sprints.component.scss'],
 })
 export class SprintsComponent implements OnInit {
+  projectId: string;
 
-  constructor() { }
+  constructor(
+    private sprintService: SprintService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe((queryParams) => {
+      this.projectId = queryParams.project;
+    });
   }
 
+  fetchActiveSprints() {
+    this.sprintService
+      .getSprintsByStatus(this.projectId, SprintStatus.ACTIVE)
+      .subscribe({
+        next: (sprints) => {
+          console.log('ACTIVE SPRINTS: ' + JSON.stringify(sprints));
+        },
+      });
+  }
 }
