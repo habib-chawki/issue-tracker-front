b9f4359740229cfd49874b7e880abd706ba764b9
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-15 11:20:39 +0100

    Unsubscribe

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
 import { ProjectFormComponent } from 'src/app/forms/project-form/project-form.component';
 import Project from 'src/app/models/project/project';
 import { ProjectCommunicationService } from 'src/app/services/project-intercom/project-intercom.service';
@@ -10,7 +12,9 @@ import { ProjectService } from 'src/app/services/project/project.service';
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, OnDestroy {
+  private unsubscribe$ = new Subject<void>();
+
   projects: Project[] = [];
 
   constructor(
@@ -20,14 +24,17 @@ export class ProjectsComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.projectService.getProjects().subscribe((response: Project[]) => {
-      this.projects = response;
-    });
+    this.projectService
+      .getProjects()
+      .pipe(take(1))
+      .subscribe((response: Project[]) => {
+        this.projects = response;
+      });
 
     // listen for project form saved announcements
-    this.projectCommunicationService.projectFormSaved$.subscribe(
-      this.createProject
-    );
+    this.projectCommunicationService.projectFormSaved$
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(this.createProject);
   }
 
   createProject = (project) => {
@@ -40,5 +47,10 @@ export class ProjectsComponent implements OnInit {
 
   onAddProject() {
     this.dialog.open(ProjectFormComponent);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 }
