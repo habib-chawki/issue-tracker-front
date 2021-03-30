82e42adee0f22f8d606d0182ab772773d7a9debd
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-30 13:26:51 +0200

    Get paginated list of users when component mounts

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { take } from 'rxjs/operators';
+import { User } from 'src/app/models/user/user';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
 export class UsersComponent implements OnInit {
+  users: User[];
+  page = 0;
+  size = 10;
 
-  constructor() { }
+  constructor(private userService: UserService) {}
 
   ngOnInit(): void {
+    // fetch users
+    this.userService
+      .getPaginatedListOfUsers(this.page, this.size)
+      .pipe(take(1))
+      .subscribe({
+        next: (response: User[]) => {
+          this.users = response;
+        },
+      });
   }
-
 }
