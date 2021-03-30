2c558b5595c10f3e251dba6b0e29a22fe14669fc
habib-chawki <touati.habib.chawki@gmail.com>
2021-03-30 11:30:51 +0200

    Add user details component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,6 +38,7 @@ import { SprintComponent } from './components/sprint/sprint.component';
 import { BoardFormComponent } from './forms/board-form/board-form.component';
 import { SprintsComponent } from './components/sprints/sprints.component';
 import { SprintDetailsComponent } from './components/sprint-details/sprint-details.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
@@ -62,6 +63,7 @@ import { SprintDetailsComponent } from './components/sprint-details/sprint-detai
     BoardFormComponent,
     SprintsComponent,
     SprintDetailsComponent,
+    UserComponent,
   ],
   imports: [
     DragDropModule,
