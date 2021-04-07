07857c5e28223c345677cedab982212cc7e62d5d
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-07 17:38:06 +0200

    Handle http errors, display snackBar

--- src/app/services/error/error.service.ts
+++ src/app/services/error/error.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ErrorService {
+  constructor(private snackBar: MatSnackBar) {}
 
-  constructor() { }
+  handleHttpError(response) {
+    this.snackBar.open(response.error.errorMessage, 'Dismiss', {
+      duration: 6000,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
 }
