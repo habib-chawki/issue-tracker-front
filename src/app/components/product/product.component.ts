2b31879c02161db7887ed05705994e40f262837c
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-19 15:20:17 +0200

    Set up queryParams type

---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import SprintStatus from 'src/app/models/enums/sprint-status';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss'],
+})
+export class ProductComponent implements OnInit {
+  queryParams = { activeSprints: {}, finishedSprints: {}, devs: {} };
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe({
+      next: (queryParams) => {
+        // extract the project id query param
+        const project = queryParams.project;
+
+        // set up each route's query params
+        this.queryParams = {
+          activeSprints: {
+            project,
+            status: SprintStatus.ACTIVE,
+          },
+          finishedSprints: {
+            project,
+            status: SprintStatus.OVER,
+          },
+          devs: { project },
+        };
+      },
+    });
+  }
+}
