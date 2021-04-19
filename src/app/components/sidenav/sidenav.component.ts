e127c0322359ac66da7a6a1fe70e5348a5d9d57d
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-19 15:15:02 +0200

    Extract project id and set up each route's query params

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import SprintStatus from 'src/app/models/enums/sprint-status';
 
 @Component({
@@ -8,55 +8,29 @@ import SprintStatus from 'src/app/models/enums/sprint-status';
   styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
-  queryParams = {
-    activeSprints: {},
-    finishedSprints: {},
-    devs: {},
-  };
+  queryParams = {};
 
-  projectId: string;
-
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    // extract the project id query param
     this.route.queryParams.subscribe({
       next: (queryParams) => {
-        this.projectId = queryParams.project;
+        // extract the project id query param
+        const project = queryParams.project;
 
+        // set up each route's query params
         this.queryParams = {
           activeSprints: {
-            project: this.projectId,
+            project,
             status: SprintStatus.ACTIVE,
           },
           finishedSprints: {
-            project: this.projectId,
+            project,
             status: SprintStatus.OVER,
           },
-          devs: { project: this.projectId },
+          devs: { project },
         };
       },
     });
   }
-
-  onDisplayActiveSprints = () => {
-    // navigate to sprints component
-    this.router.navigate(['/product/sprints'], {
-      queryParams: { project: this.projectId, status: SprintStatus.ACTIVE },
-    });
-  };
-
-  onDisplayFinishedSprints = () => {
-    // navigate to sprints component
-    this.router.navigate(['/product/sprints'], {
-      queryParams: { project: this.projectId, status: SprintStatus.OVER },
-    });
-  };
-
-  onDisplayDevs = () => {
-    // navigate to users component
-    this.router.navigate(['/product/devs'], {
-      queryParams: { project: this.projectId },
-    });
-  };
 }
