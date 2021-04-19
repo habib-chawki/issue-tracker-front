96960c8721879c9c7eea0125a786916d5e29d27b
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-19 14:04:09 +0200

    Handle navigation

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import SprintStatus from 'src/app/models/enums/sprint-status';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  projectId: string;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit(): void {
+    // extract the project id query param
+    this.route.queryParams.subscribe({
+      next: (queryParams) => {
+        this.projectId = queryParams.project;
+      },
+    });
   }
 
+  onDisplayActiveSprints = () => {
+    // navigate to sprints component
+    this.router.navigate(['/sprints'], {
+      queryParams: { project: this.projectId, status: SprintStatus.ACTIVE },
+    });
+  };
+
+  onDisplayFinishedSprints = () => {
+    // navigate to sprints component
+    this.router.navigate(['/sprints'], {
+      queryParams: { project: this.projectId, status: SprintStatus.OVER },
+    });
+  };
+
+  onDisplayDevs = () => {
+    // navigate to users component
+    this.router.navigate(['/devs'], {
+      queryParams: { project: this.projectId },
+    });
+  };
 }
