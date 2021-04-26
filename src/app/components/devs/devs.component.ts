1de91b7c0263d9e6fc60061c3c5397c0f3392f69
habib-chawki <touati.habib.chawki@gmail.com>
2021-04-26 15:54:09 +0200

    Add remove user from project handler

--- src/app/components/devs/devs.component.ts
+++ src/app/components/devs/devs.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user/user';
 
 @Component({
   selector: 'app-devs',
   templateUrl: './devs.component.html',
-  styleUrls: ['./devs.component.scss']
+  styleUrls: ['./devs.component.scss'],
 })
 export class DevsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onRemoveUserFromProject(user: User) {
+    // handle remove user from project event
   }
-
 }
