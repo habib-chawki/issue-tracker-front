0b2a3eb560f825819a0b5513e8ee683001e72364
habib-chawki <touati.habib.chawki@gmail.com>
2021-10-10 13:44:41 +0200

    Announce sprint deleted

--- src/app/services/sprint-intercom/sprint-intercom.service.ts
+++ src/app/services/sprint-intercom/sprint-intercom.service.ts
@@ -7,10 +7,15 @@ import Sprint from 'src/app/models/sprint/sprint';
 })
 export class SprintSharedService {
   sprintFormSaved$ = new Subject<Sprint>();
+  sprintDeleted$ = new Subject<Sprint>();
 
   constructor() {}
 
   announceSprintFormSaved(sprintForm: Sprint) {
     this.sprintFormSaved$.next(sprintForm);
+  }
+
+  announceSprintDeleted(sprintToDelete: Sprint) {
+    this.sprintDeleted$.next(sprintToDelete);
   }
 }
